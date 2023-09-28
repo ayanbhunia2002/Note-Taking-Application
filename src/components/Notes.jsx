be78973d9c177a5e707290f2048728714baed374
Sukriti Kuila <sukritikuila22@gmail.com>
2023-09-28 22:24:05 +0530

    Change the UI - 2

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Notes() {
   return (
     <div className="note">
-      <h1>This is the React Bootcamp</h1>
+      <h1>My Note</h1>
       <p>
-        All the students who complete the Bootcamp with complete attendance and
-        project submission will be rewarded certificates from DevTown.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Necessitatibus harum molestiae ratione hic? Fuga nam, ducimus laboriosam nisi vitae et?
       </p>
     </div>
   );
